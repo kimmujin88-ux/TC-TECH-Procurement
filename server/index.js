2207671aba038bbf7662df99e1200cc5e678619b
김무진 <kimmujin88@gmail.com>
2026-05-28 18:02:40 +0900

    feat: Supabase 직접 연결 + GitHub Pages용 docs 폴더 추가

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../docs')));
 
 // 라우터
 app.use('/api/suppliers', require('./routes/suppliers'));
@@ -19,7 +19,7 @@ app.use('/api/purchase-orders', require('./routes/purchaseOrders'));
 app.use('/api/state', require('./routes/appState'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../docs/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
